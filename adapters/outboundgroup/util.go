2a49895fbf6ffa93d19d20a5e4f7403652c36e00
duament <30264485+duament@users.noreply.github.com>
2020-03-21 11:43:38 +0800

    relay: go fmt

--- adapters/outboundgroup/util.go
+++ adapters/outboundgroup/util.go
@@ -9,40 +9,40 @@ import (
 )
 
 func addrToMetadata(rawAddress string) (addr *C.Metadata, err error) {
-        host, port, err := net.SplitHostPort(rawAddress)
-        if err != nil {
-                err = fmt.Errorf("addrToMetadata failed: %w", err)
-                return
-        }
+	host, port, err := net.SplitHostPort(rawAddress)
+	if err != nil {
+		err = fmt.Errorf("addrToMetadata failed: %w", err)
+		return
+	}
 
-        ip := net.ParseIP(host)
-        if ip != nil {
-                if ip.To4() != nil {
-                        addr = &C.Metadata{
-                                AddrType: C.AtypIPv4,
-                                Host:     "",
-                                DstIP:    ip,
-                                DstPort:  port,
-                        }
-                        return
-                } else {
-                        addr = &C.Metadata{
-                                AddrType: C.AtypIPv6,
-                                Host:     "",
-                                DstIP:    ip,
-                                DstPort:  port,
-                        }
-                        return
-                }
-        } else {
+	ip := net.ParseIP(host)
+	if ip != nil {
+		if ip.To4() != nil {
+			addr = &C.Metadata{
+				AddrType: C.AtypIPv4,
+				Host:     "",
+				DstIP:    ip,
+				DstPort:  port,
+			}
+			return
+		} else {
+			addr = &C.Metadata{
+				AddrType: C.AtypIPv6,
+				Host:     "",
+				DstIP:    ip,
+				DstPort:  port,
+			}
+			return
+		}
+	} else {
 		addr = &C.Metadata{
-                        AddrType: C.AtypDomainName,
-                        Host:     host,
-                        DstIP:    nil,
-                        DstPort:  port,
-                }
-                return
-        }
+			AddrType: C.AtypDomainName,
+			Host:     host,
+			DstIP:    nil,
+			DstPort:  port,
+		}
+		return
+	}
 }
 
 func tcpKeepAlive(c net.Conn) {
